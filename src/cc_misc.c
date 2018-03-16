d5cc2cdb86d770fb7c74abe876c7ba918c840cf9
Fillps <filipess@live.com>
2018-03-16 20:38:20 -0300

    tabela de simbolos

--- src/cc_misc.c
+++ src/cc_misc.c
@@ -1,6 +1,14 @@
+#include <stdlib.h>
+#include <memory.h>
+#include <main.h>
+#include "cc_dict.h"
 #include "cc_misc.h"
 
 extern int line_number;
+extern char* yytext;
+extern int yyleng;
+
+comp_dict_t* symbol_table;
 
 int comp_get_line_number (void)
 {
@@ -14,17 +22,37 @@ void yyerror (char const *mensagem)
 
 void main_init (int argc, char **argv)
 {
-  //implemente esta função com rotinas de inicialização, se necessário
+    symbol_table = dict_new();
 }
 
 void main_finalize (void)
 {
-  //implemente esta função com rotinas de inicialização, se necessário
+    for (int i = 0; i < symbol_table->size; i++)
+        if (symbol_table->data[i])
+            free(dict_remove(symbol_table, (symbol_table->data[i])->key));
+    dict_free(symbol_table);
 }
 
 void comp_print_table (void)
 {
   //para cada entrada na tabela de símbolos
   //Etapa 1: chame a função cc_dict_etapa_1_print_entrada
-  //implemente esta função
+    for (int i = 0; i < symbol_table->size; i++)
+        if (symbol_table->data[i])
+            cc_dict_etapa_1_print_entrada(symbol_table->data[i]->key, *((int*)symbol_table->data[i]->value));
+}
+
+void insert_symbol(void){
+    char* key = (char*)malloc(yyleng*sizeof(char));
+    int* line = (int*)malloc(sizeof(int));
+    *line = line_number;
+    if(yytext[0]=='\"' || yytext[0]=='\'') {
+        strncpy(key, yytext + 1, yyleng - 2);
+        key[yyleng-2] = '\0';
+    }
+    else
+        strcpy(key, yytext);
+    dict_remove(symbol_table, key);
+    dict_put(symbol_table, key, line);
+    free(key);
 }
